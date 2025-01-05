0d4bda863a6da70e5f9f7d532fdb0775c3529ec5
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2025-01-05 01:15:04 +0100

    inicio da criação da navbar

--- programacaoweb.com.br/tailwind.config.js
+++ programacaoweb.com.br/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neutral-1': '#212529'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
